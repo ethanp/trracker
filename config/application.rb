8503fbf5734ea28da8baa92eacc85faf70bfea9f
ethanp <ethanp@utexas.edu>
2014-07-20 01:24:22 -0700

    FIX timezone issues by setting the default timezone to my timezone. it's doing what I wanted, but I'm not sure that's really the best resolution.

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,11 @@ module Trracker
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # config.time_zone = 'Eastern Time (US & Canada)'
+    # TODO uncomment this upon arriving in Texas
     # config.time_zone = 'Central Time (US & Canada)'
+    # TODO shouldn't it be using the *user's* timezone?
+    config.time_zone = 'Pacific Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
