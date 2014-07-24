8786a1daa2f4538259459aed7947a70a42ffe6ab
ethanp <ethanp@utexas.edu>
2014-07-23 21:27:05 -0700

    improve task/show.haml

--- app/models/interval.rb
+++ app/models/interval.rb
@@ -1,14 +1,20 @@
 class Interval < ActiveRecord::Base
   belongs_to :task
 
-  # the number of seconds in this interval
   def start_time
     Time.parse(self.start.to_s)
   end
+
   def end_time
     Time.parse(self.end.to_s)
   end
+
   def seconds_spent
     self.end_time - self.start_time
+  end
+
+  # { day:int, time:int, value:float }
+  def heatmap_format
+    # TODO
   end
 end
