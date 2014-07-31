4536429e073590990ec092efe37b82383f000c95
ethanp <ethanp@utexas.edu>
2014-07-30 20:12:22 -0500

    add priority to task interface

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -97,7 +97,7 @@ class TasksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def task_params
-      params.require(:task).permit(:name, :complete, :duedate, :category_id)
+      params.require(:task).permit(:name, :complete, :duedate, :category_id, :priority)
     end
 
     def parse_duedate(p)
