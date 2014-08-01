76da6d4aeb84ca49a2f99e18cafb21b7443f6049
ethanp <ethanp@utexas.edu>
2014-08-01 16:57:34 -0500

    color tasks list according to urgency

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def pressing_duedate(task)
+    if not task.duedate.nil?
+      due = DateTime.parse(task.duedate.to_s)
+      distance = (due - DateTime.now).to_f
+      case distance
+        when 0..3
+          "danger"
+        when 3..7
+          "warning"
+      end
+    end
+  end
 end
