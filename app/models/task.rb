548b78a6b46cb1169a30cdf7ce091518527d022e
ethanp <ethanp@utexas.edu>
2014-10-29 02:09:12 -0500

    figure out how the heatmap is put together

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,12 +1,19 @@
 class Task < ActiveRecord::Base
+
   belongs_to :category
   has_many :subtasks, dependent: :destroy
   has_many :intervals, -> { order('start') }, dependent: :destroy
 
   # validations
-  validates_presence_of :name, :category_id, :priority
+  validates_presence_of   :name, :category_id, :priority
   validates_uniqueness_of :name, scope: [:category_id]
-  validates_length_of :name, maximum: 60, too_long: 'That name is too long (60 chars max)'
+  validates_length_of     :name, maximum: 60, too_long: 'That name is too long (60 chars max)'
+
+  # scopes
+  # ######
+  # These are saved SQL queries that allow you to do
+  #   Task.due_within_two_weeks
+  # to retrieve all tasks that fit the creteria
 
   scope :due_within_two_weeks, -> {
     where("tasks.duedate > ? AND tasks.duedate < ?",
@@ -16,36 +23,46 @@ class Task < ActiveRecord::Base
     where("tasks.duedate > ? AND tasks.duedate < ?",
           DateTime.now, datetime)
   }
-  scope :complete, -> { where('tasks.complete = ?', true) }
-  scope :incomplete, -> { where("tasks.complete = ? OR tasks.complete IS NULL", false) }
-  scope :turned_in, -> { where('tasks.turned_in = ?', true) }
+  scope :complete,    -> { where('tasks.complete = ?', true) }
+  scope :incomplete,  -> { where("tasks.complete = ? OR tasks.complete IS NULL", false) }
+  scope :turned_in,   -> { where('tasks.turned_in = ?', true) }
 
   def seconds_spent
-    self.intervals.inject(0) {
+    # this is a foldleft
+    self.intervals.inject(0) do
         |sum, interval| sum + interval.seconds_spent
-    }.to_i.seconds
+    end.to_i.seconds
   end
 
-  # collect all intervals' hash-representations into one array
   # TODO rename this thing with an IDE (didn't work in Sublime)
+  # collect all intervals' hash-representations into one array
+  # [{day: int[0..6], date:str['%m/%d/%y'], hour: int[1..24], value: float[0..1]}, ... ]
   def heatmap_hash_array
-    self.intervals.inject([]) { |arr, h| arr + h.heatmap_hash_array }
+    self.intervals.inject([]) do
+      |arr, h| arr + h.heatmap_hash_array
+    end
   end
 
-  # this is the heatmap_hash_array summed by :day and :hour (:date is gone)
-  # code from http://stackoverflow.com/questions/18421422
+  # this is the heatmap_hash_array (above), but
+  # summed by :day and :hour to produce
+  # [{day:int[0..6], hour:int[1..24], value:float[nil,0..]}, ... ]
+  # code from stackoverflow.com/questions/18421422
   def real_heatmap_hash_array
-    self.heatmap_hash_array.group_by do |elem|
-      # produces array [day_val, hour_val] for this hash
-      elem.values_at :day, :hour
-    end.map do |(day, hour), elem|
-      # At this point, attached to each [day_val, hour_val], we have
-      # a list of the original hashes, like {[d, h] => [{h1}, {h2}, ...], ... }
-      # So now we turn that into {[d, h] => [v1, v2, ...], ... }
+
+    # {[day, hour] => [{hash1}, {hash2}, ...], ... }
+    grouped = self.heatmap_hash_array.group_by do |elem|
+      elem.values_at :day, :hour # this gives you [day, hour]
+    end
+
+    grouped.map do |(day, hour), elem|
+
+      # turn that into {[d, h] => [v1, v2, ...], ... }
       values = elem.map{ |p| p[:value] }
-      # then if the list is non empty we sum it up
+
+      # if the list is non empty we sum it up, otherwise leave it as nil
       value = values.all? ? values.reduce(:+) : nil
-      # and create a new hash "elem" out of it for the d3 chart
+
+      # and create a new array elem out of it for the d3 chart
       { day: day, hour: hour, value: value }
     end
   end
@@ -55,6 +72,9 @@ class Task < ActiveRecord::Base
     self.heatmap_hash_array.group_by_date_and_sum_by_value(self)
   end
 
+  def hours_to_seconds d
+    d * 60 * 60
+  end
   # returns num seconds of class Seconds
   # sometimes just having static types is nice.
   def seconds_spent_today
@@ -66,29 +86,37 @@ class Task < ActiveRecord::Base
     else
       # today's hash is an array with up to one value
       # it is in units of hours, so we convert to seconds
-      total = todays_hash.first[:value] * 60 * 60
-      total.seconds
+      hours_to_seconds( todays_hash.first[:value] ).seconds
     end
   end
 
+  # e.g. duedate = my_task.duedate_as_datetime
+  # @returns :: either the date or nil
   def duedate_as_datetime
     return nil if self.duedate.nil?
     DateTime.parse(self.duedate.to_s)
   end
 
+  # float
   def duedate_distance_in_days
     return nil if self.duedate.nil?
     (self.duedate_as_datetime - DateTime.now).to_f
   end
 
+  # bool
   def due_within_a_week
     return false if self.duedate.nil?
     return self.duedate_distance_in_days < 7
   end
 
+  # produces the Bootstrap 3 css classes for items in the /tasks list
   def index_css_class
     if self.turned_in
-      nil
+      if self.complete
+        nil
+      else
+        warning
+      end
     elsif self.complete
       "success"
     elsif self.duedate.nil?
@@ -106,6 +134,10 @@ class Task < ActiveRecord::Base
   end
 
   def turned_in_css_class
-    self.turned_in ? 'turned-in' : 'not-turned-in'
+    if self.turned_in
+      'turned-in'
+    else
+      'not-turned-in'
+    end
   end
 end
